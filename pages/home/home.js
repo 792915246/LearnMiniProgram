245493cd6138a2a562f461624cd0100c2a417a1b
Mr.xiao <792915246@qq.com>
2020-07-12 01:58:26 +0800

    注册小程序和页面

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,13 +1,30 @@
 // pages/home/home.js
+// getApp()获取App()产生的示例对象
+const app = getApp()
+console.log(app.globalData.Name)
+// 注册页面
 Page({
 
   /**
-   * 页面的初始数据
+   * 页面的初始数据化
    */
   data: {
+    list: []
+  },
+
+  // 3.监听wxml相关的一些事情
+  click() {
+    console.log('我被点击了');
+
+  },
+  getUserInfo(event) {
+    console.log(event);
 
   },
 
+  /**
+   * 监听页面的生命周期函数
+   */
   /**
    * 生命周期函数--监听页面加载
    */
@@ -26,7 +43,16 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+    wx.request({
+      url: 'http://152.136.185.210:8000/api/n3/category',
+      success: (res) => {
+        console.log(res);
+        const data = res.data.data.category.list
+        this.setData({
+          list: data
+        })
+      }
+    })
   },
 
   /**
@@ -50,11 +76,15 @@ Page({
 
   },
 
+    // 监听页面的滚落
+    onPageScroll(obj) {
+      // console.log(obj);
+    },
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-
+    console.log('页面滚动到底部');
   },
 
   /**
